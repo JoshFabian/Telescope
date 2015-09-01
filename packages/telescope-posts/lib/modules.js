6158dabfd9d274b7135e5414087705fbb19550ac
Josh Fabian <contact@joshfabian.com>
2015-09-01 02:17:51 -0400

    Move post domain to second line

--- packages/telescope-posts/lib/modules.js
+++ packages/telescope-posts/lib/modules.js
@@ -37,27 +37,27 @@ Telescope.modules.add("postHeading", [
     template: 'post_title',
     order: 10
   },
-  {
-    template: 'post_domain',
-    order: 20
-  }
+  // {
+  //   template: 'post_domain',
+  //   order: 20
+  // }
 ]);
 
 Telescope.modules.add("postMeta", [
   {
-    template: 'post_author',
+    template: 'post_domain',
     order: 10
   },
-  {
-    template: 'post_info',
-    order: 20
-  },
-  {
-    template: 'post_comments_link',
-    order: 30
-  },
+  // {
+  //   template: 'post_info',
+  //   order: 20
+  // },
+  // {
+  //   template: 'post_comments_link',
+  //   order: 30
+  // },
   {
     template: 'post_admin',
-    order: 50
+    order: 20
   }
 ]);
